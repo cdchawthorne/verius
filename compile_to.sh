6233dfe344c6c950b992a014182c1497d703b9c2
Christopher Hawthorne <cdchawthorne@gmail.com>
2013-08-10 02:27:22 -0400

    Corrected dependency list, modified compile_to.sh to work with ghc 6

--- compile_to.sh
+++ compile_to.sh
@@ -7,7 +7,7 @@ else
 fi
 
 cd "$(dirname "$(realpath $0)")"
-ghc -o ${target} -hidir obj -odir obj \
+ghc --make -o ${target} -hidir obj -odir obj \
     src/Verius.hs src/DataTypes.hs src/DBInterface.hs src/HtmlGeneration/BlogHome.hs \
     src/HtmlGeneration/BlogPost.hs src/HtmlGeneration/ContentWrapping.hs \
     src/HtmlGeneration/TemplateHtmlGetter.hs
